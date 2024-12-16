f1d02ca334643c8d49c4e9e0ec1c4906451bb33b
YUKI <78786632+ykshio@users.noreply.github.com>
2024-12-16 11:55:46 +0900

    update

--- uniqa/uniqa/settings.py
+++ uniqa/uniqa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'auth.uniqa@gmail.com'  # あなたのメールアドレス
-EMAIL_HOST_PASSWORD = 'pdke nxaq uhtj ghcg'    # アプリパスワード
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # AUTH_USER_MODEL = 'users.User'
 AUTH_USER_MODEL = 'users.CustomUser'
